51470b50f711f9d1360e74832157325f6cbb61af
Joaquin Pettinari <joaquinpettinari@joaquin-mba-FXL7.local>
2024-07-29 20:14:29 -0300

    Adding pa11y

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 import express from "express";
-
+import pa11y from "pa11y";
+import { errorResponse, successResponse } from "./utils.js";
+import bodyparser from "body-parser";
 const app = express();
 
 app.listen(5001, () => console.log("listening on port 5001"));
 
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
 app.get("/", (req, res) => res.json("My api running"));
+
+app.post("/analizar", async (req, res) => {
+  const defaultIncludes = {
+    includeWarnings: true,
+    includeNotices: true,
+    timeout: 20000,
+  };
+
+  const url = req.body?.url;
+  const actions = req.body?.actions;
+  let successResponseData;
+  try {
+    if (!url) {
+      res.status(404).json({
+        error: "Sin url",
+      });
+    }
+    if (actions) {
+      defaultIncludes.actions = actions
+        .split("\n")
+        .map((line) => line.replace(/\r$/, ""))
+        .filter((line) => line.trim() !== "");
+    }
+    console.log("🧑‍🏭 Fetching...");
+    const pa11yResponse = await pa11y(url, {
+      ...defaultIncludes,
+      chromeLaunchConfig: {
+        headless: "new",
+      },
+    });
+    successResponseData = successResponse(pa11yResponse, url);
+    console.log("✅ Fetch success ");
+    res.status(200).json(successResponseData);
+  } catch (error) {
+    const errorData = errorResponse(error, url);
+    res.status(500).json(errorData);
+  }
+});
---
+++ utils.js
@@ -0,0 +1,142 @@
+const baseUrl = "https://www.w3.org/WAI/WCAG21/Techniques/";
+const defaultGuideLink = "https://www.w3.org/TR/WCAG20/";
+
+const techniqueTypeFull = {
+  ARIA: "aria",
+  SCR: "client-side-script",
+  C: "css",
+  F: "failures",
+  G: "general",
+  H: "html",
+  PDF: "pdf",
+  SVR: "server-side-script",
+  SL: "silverlight",
+  SM: "smil",
+  T: "text",
+};
+
+function parsedPrinciple(code) {
+  const parts = code.split(".");
+
+  const principio = extractNumberAfterPrefix(parts, "Principle");
+  const pauta = extractNumberAfterPrefix(parts, "Guideline");
+  const criterio = parts[3];
+  return {
+    principio: principio !== null ? parseInt(principio, 10) : null,
+    pauta: pauta !== null ? parseFloat(pauta.replace("_", ".")) : null,
+    criterio: criterio !== null ? criterio.replaceAll("_", ".") : null,
+  };
+}
+
+function extractNumberAfterPrefix(parts, prefix) {
+  const part = parts.find((p) => p.startsWith(prefix));
+  if (part) {
+    const numberPart = part.replace(prefix, "");
+    return numberPart;
+  }
+  return null;
+}
+
+function mapIssuesWithGuideLink(issues) {
+  return issues.map((issue) => {
+    const parts = issue.code.split("_").pop();
+    const secondPards = parts.split(".");
+    secondPards.shift();
+    const codes = secondPards.join(".").split(",");
+    let guideLinks = [];
+    if (!codes[0]) {
+      return { ...issue, guideLinks: [defaultGuideLink] };
+    }
+    guideLinks = codes.map((code) => {
+      const techniqueType = code.match(/[A-Z]+/)?.[0] || "";
+      const techniqueNumber = code.match(/[0-9]+/)?.[0] || "";
+      return `${baseUrl}${techniqueTypeFull[techniqueType]}/${techniqueType}${techniqueNumber}`;
+    });
+    return {
+      ...issue,
+      guideLinks,
+      parsedPrinciple: parsedPrinciple(issue.code),
+    };
+  });
+}
+
+function countIssuesByType(issues) {
+  const count = {
+    error: 0,
+    warning: 0,
+    notice: 0,
+  };
+  issues.forEach((issue) => {
+    count[issue.type]++;
+  });
+  return count;
+}
+
+function isAccessible(issues) {
+  const uniqueCodes = new Set(
+    issues.map((issue) => issue?.parsedPrinciple?.criterio)
+  );
+  return {
+    accessible: uniqueCodes.size <= 8,
+    countAprovedIssues: 38 - uniqueCodes.size,
+  };
+}
+
+const defaultErrorResponse = (textError, url) => {
+  return {
+    data: { documentTitle: "", pageUrl: url, issues: [] },
+    ok: false,
+    issueCountByType: { error: 0, warning: 0, notice: 0 },
+    accessible: false,
+    countAprovedIssues: 0,
+    error: textError,
+  };
+};
+
+const successResponse = (pa11yResponse, url) => {
+  const issues = pa11yResponse.issues;
+  const mappedIssues = mapIssuesWithGuideLink(issues);
+  const issueCountByType = countIssuesByType(issues);
+  const errorIssues = mappedIssues.filter((issue) => issue.typeCode === 1);
+  const { countAprovedIssues, accessible } = isAccessible(errorIssues);
+
+  return {
+    data: { ...pa11yResponse, issues: mappedIssues, pageUrl: url },
+    ok: true,
+    issueCountByType,
+    accessible,
+    countAprovedIssues,
+  };
+};
+
+const errorResponse = (error, url) => {
+  console.log(error);
+  if (error.message.includes("timed out")) {
+    return {
+      ...defaultErrorResponse(
+        "El análisis de la página ha tardado demasiado tiempo. Por favor, intente mas tarde.",
+        url
+      ),
+    };
+  } else if (error.name === "TypeError") {
+    return {
+      ...defaultErrorResponse(
+        `Error al parsear la URL ${url}, pruebe escribiendo http:// adelante`,
+        url
+      ),
+    };
+  } else if (error.message.includes("net::ERR_NAME_NOT_RESOLVED")) {
+    return {
+      ...defaultErrorResponse(`No se encuentra la URL: ${url}`, url),
+    };
+  } else {
+    return {
+      ...defaultErrorResponse(
+        "Error durante el análisis de la página. Por favor, intente mas tarde",
+        url
+      ),
+    };
+  }
+};
+
+export { successResponse, errorResponse };
